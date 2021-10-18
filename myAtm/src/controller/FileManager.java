4e1f7a03a433cc8f43bd99d4a2d7df22ceab71b5
jungYonghee <69584880+bupomya@users.noreply.github.com>
2021-10-18 21:03:55 +0900

    putData

--- myAtm/src/controller/FileManager.java
+++ myAtm/src/controller/FileManager.java
@@ -6,6 +6,44 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class FileManager {
+	private File file;
+	private FileWriter fw;
+	private FileReader fr;
+	private BufferedReader br;
 	
+	private String fileUserName = "users.txt";
+	private String fileAccsName = "accs.txt";
+	
+	private UserManager um = UserManager.instance;
+	private AccountManager am = AccountManager.instance;
+	
+	public static FileManager instance = new FileManager();
+	
+	
+	public String putData() {
+		String data = "";
+		for (int i = 0; i < this.am.ac.size(); i++) {
+			data += um.users.get(i).getId()+" / ";
+			data += um.users.get(i).getPw()+" / ";
+			data += um.users.get(i).getName()+" / ";
+			data += um.users.get(i).getAccCnt()+"\n";
+		}
+		return data;
+	}//putData
+	
+	public void save() {
+		//id/pw/name/code/accCnt
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}//save
+
+	
+	public void load() {
+		
+	}//load
 	
 }//FileManager
