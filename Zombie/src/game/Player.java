cf6055064d5fd95b63b0599fb0ef000e70f564b1
jungYonghee <69584880+bupomya@users.noreply.github.com>
2021-10-25 20:21:54 +0900

    Player attack

--- Zombie/src/game/Player.java
+++ Zombie/src/game/Player.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.util.Random;
+
 public class Player extends Unit{
 	private int recovery = 3;
 	
@@ -16,7 +18,7 @@ public class Player extends Unit{
 		if (recovery>0) {
 			System.out.println("체력 100회복");
 			this.setHp(this.getHp()+100);
-			System.out.printf("%s의 남은체력 : %d",this.getName(),this.getHp());
+			System.out.printf("%s의 남은체력 : %d",getName(),getHp());
 			recovery--;
 		}else {
 			System.out.println("회복횟수 초과");
@@ -24,8 +26,26 @@ public class Player extends Unit{
 	}//recoveryHp
 	
 	public void attack(Unit target) {
+		Random rn = new Random();
+		ZombieKing zombieKing = (ZombieKing) target;
 		if (target instanceof ZombieKing) {
-			
+			if (zombieKing.getShield()>0) {
+				int damage = (this.getAttack() - target.getDefense())*(rn.nextInt(150)+50)/100;
+				if (damage<=0) {
+					damage = 1;
+				}
+				System.out.printf("%s의 공격 -> %d데미지\n",getName(),damage);
+				((ZombieKing) target).setShield(zombieKing.getShield()-damage);
+				if (zombieKing.getShield()<=0) {
+					System.out.printf("%s 의 쉴드 파괴",target.getName());
+					zombieKing.setShield(0);
+				}
+				System.out.printf("%s의 남은 체력 : %d / 쉴드 : %d",target.getName(),target.getHp(),zombieKing.getShield());
+			}else {
+				super.attack(target);
+			}
+		}else {
+			super.attack(target);
 		}
 	}//attack
 }//player
--- Zombie/src/game/ZombieKing.java
+++ Zombie/src/game/ZombieKing.java
@@ -5,5 +5,12 @@ public class ZombieKing extends Unit{
 	public ZombieKing(String name,int hp,int attack,int defense,int floor,int shield) {
 		super(name, hp, attack, defense, floor);
 		this.shield = shield;
+	}//ZombieKing
+	
+	public int getShield() {
+		return this.shield;
+	}//getShield
+	public void setShield(int shield) {
+		this.shield = shield;
 	}
-}
+}//ZombieKing
